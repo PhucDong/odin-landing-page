d13ef7aee63f7e559b83d9ea59f241a5a0f57cdf
Phuc Dong <dongvuminhphuc@gmail.com>
2022-09-29 10:04:11 +0700

    Add the 1st version of projects section

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+let sum = 0;
+let newArr = [];
+ 
+function runningSum() {
+    // for (let i = 0; i < nums.length; i++) { // i=0, 1
+    //     sum += nums[i]; // sum = sum + nums[0] = 0 + 1 = 1
+    //     newArr[i] = sum; // newArr[0] = sum = 1, [1]
+    // }
+     
+    // return newArr;
+
+    sum = 8;
+    console.log(sum);
+};
+
+// runningSum();
+console.log(sum);
+// runningSum();    
+
+// runningSum([1,2,3,4]);
+// runningSum([1,1,1,1,1]);
+// let result = runningSum([3,1,2,10,1]);
+// console.log(runningSum([1,2,3,4]));
